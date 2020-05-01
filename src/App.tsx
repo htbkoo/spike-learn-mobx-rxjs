951dbe904d464e81ba3b104791cd56d15b711d4c
htbkoo <h1017760@connect.hku.hk>
2020-05-01 16:29:06 +0800

    (WIP) applied MUI styling; updated design

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
 import React from 'react';
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {createStyles, Theme} from "@material-ui/core";
+import TextField from '@material-ui/core/TextField';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import {AddCircle} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {
-            '& > *': {
-                margin: theme.spacing(1),
-                width: '25ch',
-            },
+        todoListApp: {
+            "textAlign": "center",
+            minWidth: "80%",
+            height: "80vh",
+            padding: theme.spacing(2),
+            display: "flex",
+            flexDirection: "column",
         },
-        app: {
-            "textAlign": "center"
+        todoItemsContainer: {
+            margin: theme.spacing(1),
+            overflowY: "auto",
+            flex: 1,
         },
         appContainer: {
-            "backgroundColor": "#282c34",
+            "backgroundColor": theme.palette.background.default,
             "minHeight": "100vh",
             "display": "flex",
             "flexDirection": "column",
             "alignItems": "center",
             "justifyContent": "center",
             "fontSize": "calc(10px + 2vmin)",
-            "color": "white"
+            "color": theme.palette.primary.main
         },
         appLink: {
             "color": "#61dafb"
         },
+        todoItemContainer: {
+            display: "flex",
+            padding: theme.spacing(2),
+        },
+        flexOne: {
+            flex: 1,
+        }
     }),
 );
 
@@ -34,11 +50,43 @@ const App: React.FC = () => {
 
     return (
         <div className={classes.appContainer}>
-            <div className={classes.app}>
-
-            </div>
+            <TodoListApp/>
         </div>
     );
 };
+
+function TodoListApp() {
+    const classes = useStyles();
+
+    return (
+        <Paper className={classes.todoListApp}>
+            <Typography variant="h3">Simple To Do List</Typography>
+            <form className={classes.todoItemsContainer} noValidate autoComplete="off">
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+                <TodoItem/>
+            </form>
+        </Paper>
+    )
+
+    function TodoItem() {
+        return (
+            <div className={classes.todoItemContainer}>
+                <TextField label="Todo item" variant="outlined" className={classes.flexOne}/>
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                    <AddCircle/>
+                </IconButton>
+            </div>
+        )
+    }
+}
 
 export default App;
