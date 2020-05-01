75a0aa76b30ed514484365afdc359153875c728a
htbkoo <h1017760@connect.hku.hk>
2020-05-01 16:05:46 +0800

    updated theme to dark mode

--- src/muiThemeFactory.ts
+++ src/muiThemeFactory.ts
@@ -1,13 +1,10 @@
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
 import responsiveFontSizes from "@material-ui/core/styles/responsiveFontSizes";
 
 export function createMyMuiTheme(){
     const theme = createMuiTheme({
         palette: {
-            primary: purple,
-            secondary: green,
+            type: 'dark',
         },
     });
 
