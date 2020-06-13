a4836c107e1b138f9f56d51a41d12dcc34361eec
htbkoo <h1017760@connect.hku.hk>
2020-06-13 22:10:19 +0800

    fixed broken test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {MemoryRouter as Router, Route, Switch} from "react-router-dom";
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render((
+        <Router>
+            <Switch>
+                <Route path="*">
+                    <App />
+                </Route>
+            </Switch>
+        </Router>
+    ), div);
     ReactDOM.unmountComponentAtNode(div);
 });
