bcb5393ee2898954fdbb68f8ced2b8f37a99834e
htbkoo <h1017760@connect.hku.hk>
2020-06-13 21:46:58 +0800

    added `ListItemLink`

--- src/utils/StyledRouterLink.tsx
+++ src/utils/StyledRouterLink.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Link as RouterLink, LinkProps as RouterLinkProps} from 'react-router-dom';
 import Link, {LinkProps} from '@material-ui/core/Link';
 import {Omit} from '@material-ui/types';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 export type StyledRouterLinkProps = { to: RouterLinkProps['to'], children: React.ReactNode, } & LinkProps;
 
@@ -11,6 +14,34 @@ export default function StyledRouterLink({to, children, ...linkProps}: StyledRou
         <Link component={RouterLink} to={to as any} {...linkProps}>
             {children}
         </Link>
+    );
+}
+
+
+interface ListItemLinkProps {
+    icon?: React.ReactElement;
+    primary: string;
+    to: string;
+}
+
+export function ListItemLink(props: ListItemLinkProps) {
+    const {icon, primary, to} = props;
+
+    const renderLink = useMemo(
+        () =>
+            React.forwardRef<any, Omit<RouterLinkProps, 'to'>>((itemProps, ref) => (
+                <RouterLink to={to} ref={ref} {...itemProps} />
+            )),
+        [to],
+    );
+
+    return (
+        <li>
+            <ListItem button component={renderLink}>
+                {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+                <ListItemText primary={primary} />
+            </ListItem>
+        </li>
     );
 }
 
