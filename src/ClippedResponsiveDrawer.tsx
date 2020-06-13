817fbc9047d6052b8dfaddae86247b38372dafda
htbkoo <h1017760@connect.hku.hk>
2020-06-13 21:57:15 +0800

    removed unnecessary `Divider` component

--- src/ClippedResponsiveDrawer.tsx
+++ src/ClippedResponsiveDrawer.tsx
@@ -66,7 +66,6 @@ export default function ClippedResponsiveDrawer(
     const drawer = (
         <div>
             <Toolbar />
-            <Divider />
             {drawerContent}
         </div>
     );
