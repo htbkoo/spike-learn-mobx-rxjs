093f1a2a9035a83ceb153609abee51d7afe8d1eb
htbkoo <h1017760@connect.hku.hk>
2020-06-14 13:34:58 +0800

    cleaned up

--- src/minesweeper/PlainReactHookMinesweeperApp.tsx
+++ src/minesweeper/PlainReactHookMinesweeperApp.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {createStyles, Theme} from "@material-ui/core";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Button from "@material-ui/core/Button";
 import {range} from "lodash";
 
