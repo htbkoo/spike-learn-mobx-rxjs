663f389962432e34a40e60bbd7674bd585be05e7
Broderick Koo <broderick.koo@cloverhealth.com>
2020-07-27 12:10:22 +0800

    added `utils/hooks.tsx`

---
+++ src/utils/hooks.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+// reference: https://stackoverflow.com/a/34425083
+const useScript = url => {
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.src = url;
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, [url]);
+};
+
+export default useScript;
